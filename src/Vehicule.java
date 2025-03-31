460c963b91f658b29ab60f060beebe1bd21ca7cd
Melvin MARTINVALET <melvin.ma@kuikops.com>
2025-03-31 15:39:24 +0200

    commit initial TP Java

---
+++ src/Vehicule.java
@@ -0,0 +1,24 @@
+public class Vehicule {
+    private String marque;
+    private String modele;
+    private int annee;
+    public Vehicule (String marque, String modele,int annee) {
+        this.marque = marque;
+        this.modele = modele;
+        this.annee = annee;
+    }
+    public String getMarque() {
+        return marque;
+    }
+    public String getModele() {
+        return modele;
+    }
+    public int getAnnee() {
+        return annee;
+    }
+
+    @Override
+    public String toString() {
+        return marque + " " + modele + "(" + annee +")";
+    }
+}
